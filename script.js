f85c1e5cb304ad3942bec33ab6f243b4aedbc6a3
AliFiyouz <Alifiyouz007@gmail.com>
2022-06-09 21:49:43 +0430

    firstStage

---
+++ script.js
@@ -0,0 +1,6 @@
+let  saturday = document.querySelector(".saturday");
+let input = document.createElement("input");
+let check = document.createElement("input");
+let set = check.setAtterbiut("type","check-box");
+saturday.appendChild(check);
+saturday.appendChild(input);
